dac3cafa4381950082d30ee49ebc2788fbf1c717
Krishan Bandara <34151165+krishanb4@users.noreply.github.com>
2023-10-05 23:30:06 +0530

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,303 @@
+import type { NextPage } from "next";
+import styles from "./index.module.css";
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.home}>
+      <div className={styles.footer}>
+        <div className={styles.footerChild} />
+        <div className={styles.getSocial}>GET SOCIAL</div>
+        <img
+          className={styles.socialMedia59952669607201Icon}
+          alt=""
+          src="/socialmedia5995266-960-720-1@2x.png"
+        />
+        <img
+          className={styles.socialMedia59952669607202Icon}
+          alt=""
+          src="/socialmedia5995266-960-720-2@2x.png"
+        />
+        <div className={styles.location}>LOCATION</div>
+        <div className={styles.sanasumaBeachChaletContainer}>
+          <p className={styles.sanasumaBeachChalet}>Sanasuma Beach Chalet,</p>
+          <p className={styles.sanasumaBeachChalet}>Yala Road, Gurugoda,</p>
+          <p className={styles.sanasumaBeachChalet}>Kirinda,</p>
+          <p className={styles.sanasumaBeachChalet}>Sri Lanka</p>
+          <p className={styles.p}>+33766881344</p>
+        </div>
+        <div className={styles.footerItem} />
+      </div>
+      <div className={styles.gallerySection}>
+        <div className={styles.gallerySectionChild} />
+        <div className={styles.animalsGallery}>ANIMALS GALLERY</div>
+        <div className={styles.yalaNationalPark}>
+          Yala National Park, just a stone's throw away from Senasuma, offers a
+          captivating wildlife experience. Here, you can spot a diverse array of
+          animals, including majestic elephants, elusive leopards, graceful
+          deer, colourful birds, and curious crocodiles. With its rich
+          biodiversity, Yala is a haven for nature enthusiasts and wildlife
+          lovers, promising thrilling encounters with some of Sri Lanka's most
+          iconic creatures in their natural habitat.
+        </div>
+        <div className={styles.yalasEnigmaticLeopards}>
+          Yala's Enigmatic Leopards: Discover the thrill of encountering these
+          elusive and magnificent creatures in their natural habitat. Yala
+          National Park is renowned for its thriving leopard population,
+          offering a unique opportunity to witness these stealthy big cats in
+          action. Our guided safaris at Senasuma provide you with the best
+          chance to catch a glimpse of these stunning leopards as they prowl
+          through the wild, making your visit to Yala truly unforgettable.
+        </div>
+        <div className={styles.yalaNationalPark1}>
+          Yala National Park is home to a vibrant population of gentle giants -
+          the Asian elephants. At Senasuma, you can embark on thrilling safaris
+          to witness these magnificent creatures up close. Marvel at their
+          impressive presence and observe their natural behaviors as they roam
+          freely in their lush, jungle surroundings. A rendezvous with Yala's
+          elephants promises to be a heartwarming and awe-inspiring experience
+          you'll cherish forever.
+        </div>
+        <b className={styles.majesticEncountersWith}>
+          MAJESTIC ENCOUNTERS WITH YALA'S ELEPHANTS
+        </b>
+        <img
+          className={styles.gallerySectionItem}
+          alt=""
+          src="/rectangle-21@2x.png"
+        />
+        <img
+          className={styles.gallerySectionInner}
+          alt=""
+          src="/rectangle-28@2x.png"
+        />
+        <img
+          className={styles.rectangleIcon}
+          alt=""
+          src="/rectangle-31@2x.png"
+        />
+        <img
+          className={styles.gallerySectionChild1}
+          alt=""
+          src="/rectangle-37@2x.png"
+        />
+        <img
+          className={styles.gallerySectionChild2}
+          alt=""
+          src="/rectangle-40@2x.png"
+        />
+        <img
+          className={styles.gallerySectionChild3}
+          alt=""
+          src="/rectangle-41@2x.png"
+        />
+        <img
+          className={styles.gallerySectionChild4}
+          alt=""
+          src="/rectangle-42@2x.png"
+        />
+        <img
+          className={styles.gallerySectionChild5}
+          alt=""
+          src="/rectangle-44@2x.png"
+        />
+        <img
+          className={styles.gallerySectionChild6}
+          alt=""
+          src="/rectangle-46@2x.png"
+        />
+        <img
+          className={styles.gallerySectionChild7}
+          alt=""
+          src="/rectangle-47@2x.png"
+        />
+        <img
+          className={styles.gallerySectionChild8}
+          alt=""
+          src="/rectangle-48@2x.png"
+        />
+        <img
+          className={styles.gallerySectionChild9}
+          alt=""
+          src="/rectangle-38@2x.png"
+        />
+        <img
+          className={styles.gallerySectionChild10}
+          alt=""
+          src="/rectangle-43@2x.png"
+        />
+        <img
+          className={styles.gallerySectionChild11}
+          alt=""
+          src="/rectangle-25@2x.png"
+        />
+        <img
+          className={styles.gallerySectionChild12}
+          alt=""
+          src="/rectangle-26@2x.png"
+        />
+        <img
+          className={styles.gallerySectionChild13}
+          alt=""
+          src="/rectangle-27@2x.png"
+        />
+        <img
+          className={styles.gallerySectionChild14}
+          alt=""
+          src="/rectangle-22@2x.png"
+        />
+        <img
+          className={styles.gallerySectionChild15}
+          alt=""
+          src="/rectangle-23@2x.png"
+        />
+        <img
+          className={styles.gallerySectionChild16}
+          alt=""
+          src="/rectangle-39@2x.png"
+        />
+        <div className={styles.discoverTheEpitome}>
+          Discover the epitome of luxury at our exquisite resort. Welcome to a
+          world where breathtaking beach views, captivating wildlife encounters,
+          and sumptuous accommodations seamlessly converge.
+        </div>
+      </div>
+      <div className={styles.bodySection}>
+        <img
+          className={styles.photo2023093015151}
+          alt=""
+          src="/photo-20230930-1515-1@2x.png"
+        />
+        <div className={styles.bodySectionChild} />
+        <div className={styles.bodySectionItem} />
+        <div className={styles.ourAccommodationsAre}>
+          Our accommodations are thoughtfully designed to provide spacious
+          comfort and convenience for your entire clan. Experience the joy of
+          togetherness in the heart of Yala, where every member can unwind and
+          make cherished memories. With ample room to spread out, modern
+          amenities, and easy access to the wonders of Yala National Park, our
+          family accommodations offer the perfect retreat for your next
+          adventure. Discover the magic of family bonding at Senasuma – your
+          home away from home.
+        </div>
+        <div
+          className={styles.indulgeInThe}
+        >{`Indulge in the ultimate seaside dining experience as you savour delectable cuisine while gazing at the azure waters of the ocean. Our beachside view is more than just a picturesque backdrop; it's a tranquil haven where you can unwind, connect with `}</div>
+        <div className={styles.natureAndCreate}>
+          nature, and create lasting memories with loved ones. Whether you're
+          basking in the golden sunsets or stargazing under the open sky,
+          Senasuma's beachside view promises an unforgettable and enchanting
+          environment.
+        </div>
+        <img
+          className={styles.bodySectionInner}
+          alt=""
+          src="/rectangle-14@2x.png"
+        />
+        <img
+          className={styles.bodySectionChild1}
+          alt=""
+          src="/rectangle-15@2x.png"
+        />
+        <img
+          className={styles.bodySectionChild2}
+          alt=""
+          src="/rectangle-16@2x.png"
+        />
+        <img
+          className={styles.bodySectionChild3}
+          alt=""
+          src="/rectangle-18@2x.png"
+        />
+        <img
+          className={styles.bodySectionChild4}
+          alt=""
+          src="/rectangle-17@2x.png"
+        />
+        <div className={styles.rectangleDiv} />
+        <div className={styles.senasumaWhereComfortContainer}>
+          <b>
+            <span>SENASUMA</span>
+          </b>
+          <span className={styles.whereComfortMeetsLuxury}>
+            <b>,</b>
+            <span> WHERE COMFORT MEETS LUXURY</span>
+          </span>
+        </div>
+        <div className={styles.yourDestinationFor}>
+          YOUR DESTINATION FOR RELAXATION
+        </div>
+        <div className={styles.familyRoomsOverlayBg} />
+        <img
+          className={styles.familyRoomsBg}
+          alt=""
+          src="/family-rooms--bg@2x.png"
+        />
+        <div className={styles.experienceUnforgettableFamil}>
+          Experience unforgettable family moments in our spacious and
+          comfortable accommodations designed to cater to all your needs.
+          Discover the joy of quality time together amidst the natural wonders
+          of Yala.
+        </div>
+        <div className={styles.familyRooms}>FAMILY ROOMS</div>
+        <div className={styles.roomsSuitsBg} />
+        <div className={styles.openAirImgFrame} />
+        <img className={styles.openAirImg} alt="" src="/open-air-img@2x.png" />
+        <div className={styles.lobbyImgFrame} />
+        <img className={styles.lobbyImgIcon} alt="" src="/lobby-img@2x.png" />
+        <div className={styles.bedImgFrame} />
+        <img className={styles.bedImgIcon} alt="" src="/bed-img@2x.png" />
+        <div className={styles.roomsSuits}>{`ROOMS & SUITS`}</div>
+        <b className={styles.yourStayYour}>YOUR STAY, YOUR WAY.</b>
+      </div>
+      <div className={styles.heroSection}>
+        <div className={styles.logoSectionBg} />
+        <div className={styles.luxurySuitesFor}>
+          LUXURY SUITES FOR PEOPLE ON THE GO
+        </div>
+        <img
+          className={styles.companyLogoIcon}
+          alt=""
+          src="/company-logo@2x.png"
+        />
+        <div className={styles.descriptionBg} />
+        <div
+          className={styles.senasumaWhereLuxury}
+        >{`Senasuma, where luxury meets nature in the heart of Yala. Our enchanting hotel is a harmonious blend of contemporary comfort and the raw beauty of Yala's wilderness. Wake up to the melodies of exotic birds, savour exquisite cuisine that captures the `}</div>
+        <div className={styles.essenceOfSri}>
+          essence of Sri Lanka, and unwind in stylish, eco-friendly
+          accommodations designed for your utmost relaxation. At Senasuma, we
+          redefine your Yala experience, offering an escape where adventure and
+          comfort converge seamlessly.
+        </div>
+      </div>
+      <div className={styles.header}>
+        <img
+          className={styles.headerImageIcon}
+          alt=""
+          src="/header-image@2x.png"
+        />
+        <div className={styles.welcomeToSenasumaContainer}>
+          <p className={styles.p}>
+            <span className={styles.welcomeToSenasuma1}>
+              <span>{`WELCOME TO `}</span>
+              <span className={styles.senasuma}>{`SENASUMA `}</span>
+            </span>
+          </p>
+          <p className={styles.whereEveryStay}>
+            ‘’Where Every Stay is a Story"
+          </p>
+        </div>
+        <div className={styles.beyondTheExpectationContainer}>
+          <p className={styles.p}>{`BEYOND THE `}</p>
+          <p className={styles.p}>EXPECTATION</p>
+        </div>
+        <div className={styles.rectangleParent}>
+          <div className={styles.groupChild} />
+          <b className={styles.bookNow}>Book Now</b>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
